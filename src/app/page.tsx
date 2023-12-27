7e27285b865ca104b76f152bcd42c89f36abc3d0
Tom <Ghastly.pixels@gmail.com>
2023-12-27 13:55:30 +0000

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     text: "from testing shit, possibly viable?",
   });
 
+  console.log(hello);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
