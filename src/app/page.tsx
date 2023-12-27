b6a79717ceae9efd5118127f69da3a8fd2e76360
Tom <Ghastly.pixels@gmail.com>
2023-12-27 13:55:12 +0000

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,9 +39,7 @@ export default function Home() {
           </Link>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello?.greeting : "Loading tRPC query..."}
-          </p>
+          <p className="text-2xl text-white">"Loading tRPC query..."</p>
         </div>
       </div>
     </main>
