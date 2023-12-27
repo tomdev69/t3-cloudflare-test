1fd35d9e64aacc4f3b0e4919f720f27d9c30ec7a
Tom <Ghastly.pixels@gmail.com>
2023-12-27 13:44:12 +0000

    .

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { postRouter } from "@/server/api/routers/post";
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -7,12 +7,7 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: publicProcedure
-  .query(({ }) => {
-    return {
-      greeting: `Hello`,
-    };
-  }),
+  post: postRouter,
 });
 
 // export type definition of API
